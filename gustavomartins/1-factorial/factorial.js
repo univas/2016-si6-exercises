2f72494591aa66466abdc08cb1b013eba41c85e2
Gustavo Martins <gustavo_sh8@hotmail.com>
2016-09-15 20:30:44 -0300

    Added factorial ()

--- gustavomartins/1-factorial/factorial.js
+++ gustavomartins/1-factorial/factorial.js
@@ -1,10 +1,17 @@
 function getNumber (){
     var number = process.argv.slice(2);
-    showNumber(number);
+    showNumber(factorial(number));
 }
 
 function showNumber (number){
     console.log("Number: " + number);
+}
+
+function factorial(number) {
+    for (var i=number; i > 1; i--) {
+        number = number * (i - 1);
+    }
+    return number;
 }
 
 // init
