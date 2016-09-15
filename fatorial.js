353b14e53622f3d558c5bfc5e0c5d4728e5d96c0
Maycon Ferreira <maycon.univas@gmail.com>
2016-09-15 19:55:34 -0300

    começando

---
+++ fatorial.js
@@ -0,0 +1,19 @@
+
+(function showResult(){
+    console.log("Fatorial"+   );
+}
+
+
+
+
+
+for (number = 0; number < number; number++){
+    
+}
+
+
+fuction pegaNumero(){
+    
+    var number = getArguments()
+    .map(stringToNumero);
+}
