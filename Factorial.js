1fd078877a427da0a71588ae93e30debc6dd1399
Wellington007 <well.caldeira@gmail.com>
2016-09-21 22:47:15 -0300

    Ok

--- Factorial.js
+++ Factorial.js
@@ -1,10 +1,10 @@
 function factorial(num)
 {
-    // If the number is less than 0, reject it.
+    
     if (num < 0) {
         return -1;
     }
-    // If the number is 0, its factorial is 1.
+    
     else if (num == 0) {
         return 1;
     }
